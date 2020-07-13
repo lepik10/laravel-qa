d66ae7480f511be23f736cf8b9b4a0a37e626316
Vadim Zaytsev <lepik@MacBook-Pro-Vadim.local>
2020-07-13 09:44:19 +0400

    Make seeds for Users and Questions

--- app/User.php
+++ app/User.php
@@ -42,11 +42,4 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Question');
     }
-
-    public function setTitleAttribute($value)
-    {
-        $this->attributes['title'] = $value;
-        $this->attributes['slug'] = Str::slug($value);
-    }
-
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        factory('App\User', 3)->create()->each(function($u) {
+            $u->questions()->saveMany(factory('App\Question', rand(1, 5))->make());
+        });
+
     }
 }
