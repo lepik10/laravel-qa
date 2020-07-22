d2442c891228690aa578db3feda8afcd3e8284ec
Vadim Zaytsev <lepik@MacBook-Pro-Vadim.local>
2020-07-22 09:22:11 +0400

    Add update answer

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Policies\QuestionPolicy;
+use App\Policies\AnswerPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -14,7 +15,9 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        Question::class => QuestionPolicy::class
+        Question::class => QuestionPolicy::class,
+        Answer::class => AnswerPolicy::class
+
     ];
 
     /**
