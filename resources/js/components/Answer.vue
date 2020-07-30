68c6b3d8e1c823bc0bfc8627f1f22d6d8f66aca9
Vadim Zaytsev <lepik@MacBook-Pro-Vadim.local>
2020-07-30 16:18:56 +0400

    Create Answers component in Vue

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -1,5 +1,33 @@
 <template>
-
+    <div class="media post">
+        <div class="d-flex flex-column vote-controls">
+            <vote :model="answer" name="answer"></vote>
+        </div>
+        <div class="media-body">
+            <form v-if="editing" @submit.prevent="update">
+                <div class="form-group">
+                    <textarea class="form-control" v-model="body" rows="10" required></textarea>
+                </div>
+                <button class="btn btn-primary" type="submit" :disabled="isInvalid">Update</button>
+                <button class="btn btn-outline-primary" type="button" @click="cancel">Cancel</button>
+            </form>
+            <div v-else>
+                <div v-html="body"></div>
+                <div class="row">
+                    <div class="col-4">
+                        <div class="ml-auto">
+                            <a v-if="authorize('modify', answer)" @click.prevent="edit" class="btn btn-sm btn-outline-info">Edit</a>
+                            <button v-if="authorize('modify', answer)" @click="destroy" class="btn btn-sm btn-outline-danger">Delete</button>
+                        </div>
+                    </div>
+                    <div class="col-4"></div>
+                    <div class="col-4">
+                        <user-info :model="answer" label="answered"></user-info>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
---
+++ resources/js/components/Answers.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="row justify-content-center" v-cloak v-if="count">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h2>{{ title }} answers</h2>
+                    </div>
+                    <hr>
+
+                    <answer v-for="answer in answers" :answer="answer" :key="answer.id"></answer>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Answer from "./Answer";
+
+    export default {
+        props: ['answers', 'count'],
+        computed: {
+            title() {
+                return this.count;
+            }
+        },
+        components: {
+            Answer
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
